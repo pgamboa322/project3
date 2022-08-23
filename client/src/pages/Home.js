5d30f8959ef3170864e9db466e931fe443d0f2d2
pgamboa322 <pgamboa322@gmail.com>
2022-08-22 20:36:40 -0500

    Lowered react version to 17, built out structure of ui pages and components.

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MenuBar from "../components/MenuBar";
+
+const Home = () => {
+
+    return (
+        <>
+            <MenuBar/>
+        </>
+    )
+}
+
+export default Home;
