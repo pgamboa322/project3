7760a9680e724cbea04712a671eddf1cf02d6db8
pgamboa322 <pgamboa322@gmail.com>
2022-08-23 22:19:09 -0500

    finished dashboard page.

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,12 +1,53 @@
 import React from "react";
+import { Row, Col, Card, Button } from "react-bootstrap";
+import Ruby from "../assets/ruby.png";
 
 const Dashboard = () => {
-
-    return (
-        <>
-        <p>My Dashboard</p>
-        </>
-    )
-}
+  return (
+    <>
+      <br />
+      <Row>
+        <br />
+        <h3>Your Posts and Comments</h3>
+        <br />
+        <br />
+        <Col md={2}></Col>
+        <Col md={4}>
+          <Card className="cardDiv">
+            <Card.Img variant="top" src={Ruby} />
+            <Card.Body>
+              <Card.Title>Ruby</Card.Title>
+              <Card.Text>
+                Ruby was last seen on October, 23 2021. She is very friendly,
+                and has a black leather collar.
+              </Card.Text>
+              <Button variant="secondary">See Contact Info</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={4}>
+        <Card>
+            <Card.Header>Date Posted: 10/23/21 @ 10:50 AM</Card.Header>
+            <Card.Body>
+              <Card.Text>
+                Our sweet Ruby went missing last night, sharing her post here!
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <br/>
+          <Card>
+            <Card.Header>Date Posted: 8/11/22 @ 8:17 AM</Card.Header>
+            <Card.Body>
+              <Card.Text>
+                What a sweet baby! I hope she finds her home
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={2}></Col>
+      </Row>
+    </>
+  );
+};
 
 export default Dashboard;
